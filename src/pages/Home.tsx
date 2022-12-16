97db727669de772fbe6ecd7b890ad8a476647bc3
yoojene <eugene.cross@gmail.com>
2022-12-16 08:44:22 +0000

    feat: add modal testing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,19 @@
 import MessageListItem from '../components/MessageListItem';
-import { useState } from 'react';
+import { createRef, useEffect, useRef, useState } from 'react';
 import { Message, getMessages } from '../data/messages';
 import {
+  IonAvatar,
+  IonButton,
+  IonButtons,
   IonContent,
+  IonFooter,
   IonHeader,
+  IonImg,
+  IonInput,
+  IonItem,
+  IonLabel,
   IonList,
+  IonModal,
   IonPage,
   IonRefresher,
   IonRefresherContent,
@@ -12,6 +21,8 @@ import {
   IonToolbar,
   useIonViewWillEnter
 } from '@ionic/react';
+import { Keyboard } from '@capacitor/keyboard';
+
 import './Home.css';
 
 const Home: React.FC = () => {
@@ -29,8 +40,37 @@ const Home: React.FC = () => {
     }, 3000);
   };
 
+  const modal = useRef<HTMLIonModalElement>(null);
+  const page = useRef(null);
+
+  const [presentingElement, setPresentingElement] = useState<HTMLElement | null>(null);
+
+  useEffect(() => {
+    setPresentingElement(page.current);
+  }, []);
+
+  function dismiss() {
+    modal.current?.dismiss();
+  }
+
+    const contentRef = createRef<HTMLIonContentElement>();
+
+
+  Keyboard.addListener('keyboardDidShow', (info: any) => {
+    console.log('keyboard did show with height:', info.keyboardHeight);
+            contentRef.current?.scrollToBottom(500);
+
+
+  });
+
+  Keyboard.addListener('keyboardDidHide', () => {
+  console.log('keyboard did hide');
+          contentRef.current?.scrollToTop(500);
+
+});
+
   return (
-    <IonPage id="home-page">
+    <IonPage  ref={page} id="home-page">
       <IonHeader>
         <IonToolbar>
           <IonTitle>Inbox</IonTitle>
@@ -49,9 +89,194 @@ const Home: React.FC = () => {
           </IonToolbar>
         </IonHeader>
 
+      <IonButton  id="open-modal">
+Open modal
+        </IonButton>
+
         <IonList>
           {messages.map(m => <MessageListItem key={m.id} message={m} />)}
         </IonList>
+
+        <IonModal ref={modal} trigger="open-modal" presentingElement={presentingElement!}>
+          <IonHeader>
+            <IonToolbar>
+              <IonTitle>Modal</IonTitle>
+              <IonButtons slot="end">
+                <IonButton onClick={() => dismiss()}>Close</IonButton>
+              </IonButtons>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent className="ion-padding" ref={contentRef}>
+            <IonList>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src="https://i.pravatar.cc/300?u=b" />
+                </IonAvatar>
+                <IonLabel position="stacked">
+                  <h2>Connor Smith</h2>
+                  <p>Sales Rep</p>
+                </IonLabel>
+                <IonInput></IonInput>
+              </IonItem>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src="https://i.pravatar.cc/300?u=a" />
+                </IonAvatar>
+                <IonLabel position="stacked">
+                  <h2>Daniel Smith</h2>
+                  <p>Product Designer</p>
+                </IonLabel>               
+                 <IonInput></IonInput>
+              </IonItem>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src="https://i.pravatar.cc/300?u=d" />
+                </IonAvatar>
+                <IonLabel position="stacked">
+                  <h2>Greg Smith</h2>
+                  <p>Director of Operations</p>
+              </IonLabel>
+            </IonItem>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src="https://i.pravatar.cc/300?u=e" />
+                </IonAvatar>
+                <IonLabel position="stacked">
+                  <h2>Zoey Smith</h2>
+                  <p>CEO</p>
+                </IonLabel>                
+                <IonInput></IonInput>
+              </IonItem>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src="https://i.pravatar.cc/300?u=b" />
+                </IonAvatar>
+                <IonLabel position="stacked">
+                  <h2>Connor Smith</h2>
+                  <p>Sales Rep</p>
+                </IonLabel>                
+                <IonInput></IonInput>
+              </IonItem>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src="https://i.pravatar.cc/300?u=a" />
+                </IonAvatar>
+                <IonLabel position="stacked">
+                  <h2>Daniel Smith</h2>
+                  <p>Product Designer</p>
+                </IonLabel>                
+                <IonInput></IonInput>
+              </IonItem>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src="https://i.pravatar.cc/300?u=d" />
+                </IonAvatar>
+                <IonLabel position="stacked">
+                  <h2>Greg Smith</h2>
+                  <p>Director of Operations</p>
+                </IonLabel>                
+                <IonInput></IonInput>
+              </IonItem>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src="https://i.pravatar.cc/300?u=e" />
+                </IonAvatar>
+                <IonLabel position="stacked">
+                  <h2>Zoey Smith</h2>
+                  <p>CEO</p>
+                </IonLabel>               
+                 <IonInput></IonInput>
+              </IonItem>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src="https://i.pravatar.cc/300?u=b" />
+                </IonAvatar>
+                <IonLabel position="stacked">
+                  <h2>Connor Smith</h2>
+                  <p>Sales Rep</p>
+                </IonLabel>                
+                <IonInput></IonInput>
+              </IonItem>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src="https://i.pravatar.cc/300?u=a" />
+                </IonAvatar>
+                <IonLabel position="stacked">
+                  <h2>Daniel Smith</h2>
+                  <p>Product Designer</p>
+                </IonLabel>                
+                <IonInput></IonInput>
+              </IonItem>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src="https://i.pravatar.cc/300?u=d" />
+                </IonAvatar>
+                <IonLabel position="stacked">
+                  <h2>Greg Smith</h2>
+                  <p>Director of Operations</p>
+                </IonLabel>               
+               <IonInput></IonInput>
+              </IonItem>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src="https://i.pravatar.cc/300?u=e" />
+                </IonAvatar>
+                <IonLabel position="stacked">
+                  <h2>Zoey Smith</h2>
+                  <p>CEO</p>
+                </IonLabel>               
+                 <IonInput></IonInput>
+              </IonItem>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src="https://i.pravatar.cc/300?u=b" />
+                </IonAvatar>
+                <IonLabel position="stacked">
+                  <h2>Connor Smith</h2>
+                  <p>Sales Rep</p>
+                </IonLabel>                
+                <IonInput></IonInput>
+              </IonItem>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src="https://i.pravatar.cc/300?u=a" />
+                </IonAvatar>
+                <IonLabel position="stacked">
+                  <h2>Daniel Smith</h2>
+                  <p>Product Designer</p>
+                </IonLabel>                
+                <IonInput></IonInput>
+              </IonItem>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src="https://i.pravatar.cc/300?u=d" />
+                </IonAvatar>
+                <IonLabel position="stacked">
+                  <h2>Greg Smith</h2>
+                  <p>Director of Operations</p>
+                </IonLabel>                
+                <IonInput></IonInput>
+              </IonItem>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src="https://i.pravatar.cc/300?u=e" />
+                </IonAvatar>
+                <IonLabel position="stacked">
+                  <h2>Zoey Smith</h2>
+                  <p>CEO</p>
+                </IonLabel>                
+                <IonInput></IonInput>
+              </IonItem>
+             
+            </IonList>
+              <IonFooter>
+                        <IonToolbar>
+          <IonTitle>Footer</IonTitle>
+        </IonToolbar>
+              </IonFooter>
+          </IonContent>
+         
+        </IonModal>
       </IonContent>
     </IonPage>
   );
